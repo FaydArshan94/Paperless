aaad46ca36f26960aee81392f41926ae88aea499
fayarsh <faydarsh2000@gmail.com>
2025-08-16 11:38:49 +0530

    check

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
 import ThemeToggle from "./components/ThemeToggle";
 const App = () => {
   return (
     <div>
-      <h1 className="text-9xl text-center">Hello</h1>
+      <h1 className='text-9xl text-center'>Hello</h1>
+       <h1>Hello Dark & Light 🌗</h1>
+      <ThemeToggle />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
