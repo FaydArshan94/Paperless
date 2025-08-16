4ae2cae3c6d378d5123a2705e7e18187247e85cc
fayarsh <faydarsh2000@gmail.com>
2025-08-16 11:29:24 +0530

    f

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Paperless',
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
